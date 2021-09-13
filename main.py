7281ed6d96f75b6c839c3fbd8e02a34b63e9c705
Qohey <shimad.sh9@gmail.com>
2021-09-13 13:08:00 +0900

    基本的なエラー処理や初期化の構築

--- main.py
+++ main.py
@@ -1,34 +1,57 @@
 # coding: UTF-8
+
 import sys
 import os
+from icecream.icecream import colorize
 import numpy as np
 import pandas as pd
 from utils.option import Option
 from icecream import ic
 
+def init(opt):
+    """
+    出力用のディレクトリやファイル作成，その他諸々の初期化
+    """
+    output_file = os.path.join(opt.output_dir, opt.output_name) + ".csv" # */*.csvの形
+    if not os.path.exists(opt.output_dir):
+        os.makedirs(opt.output_dir)
+    if not os.path.isfile(output_file):
+        with open(output_file, "w", encoding="UTF-8") as f:
+            f.write("")
+    else: #ファイルが重複したときは上書き確認
+        print(output_file + "はすでに存在しています。上書きしますか？Y/n")
+        if input() == "Y":
+            with open(output_file, "w", encoding="UTF-8") as f:
+                f.write("")
+        else:
+            exit()
 
 def valid_path(opt):
-    errs = []
+    """
+    オプションで指定されたpathの検証
+    """
+    errors = []
+    input_file = os.path.join(opt.input_dir, opt.input_name) # */*.csvの形
     if opt.input_name == "":
-        errs.append("input_nameを指定してください")
-    if not os.path.exists(os.path.join(opt.input_dir, opt.input_name)):
-        errs.append(os.path.join(opt.input_dir, opt.input_name) + "が見つかりません")
-    elif not os.path.join(opt.input_dir, opt.input_name).endswith(".csv"):
-        errs.append(os.path.join(opt.input_dir, opt.input_name) + "はcsvファイルではありません")
-    if len(errs):
-        print("＊＊＊＊エラー＊＊＊＊")
-        for err in errs:
+        errors.append("input_nameを指定してください")
+    elif not os.path.isfile(input_file):
+        errors.append(input_file + "が見つかりません")
+    if 0 < len(errors):
+        print("==================Errors==================")
+        for err in errors:
             print(err)
+        print("==========================================\n")
         exit()
 
 
 def main(opt):
     target_data = pd.DataFrame()
     target_data = pd.read_csv(os.path.join(opt.input_dir, opt.input_name))
-    print(target_data)
+    print(target_data.describe())
 
 
 if __name__ == "__main__":
     option = Option().parse()
     valid_path(option)
+    init(option)
     main(option)
