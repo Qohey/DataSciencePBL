05060ae8308df8db6c2a31a1cc17f9e07d95e198
Qohey <shimad.sh9@gmail.com>
2021-09-13 13:10:18 +0900

    IO関係のオプションの追加や前提のフォルダ構造を作成するように変更

--- utils/option.py
+++ utils/option.py
@@ -1,5 +1,8 @@
 # coding: UTF-8
+
+import os
 import argparse
+from pprint import pprint
 
 class Option():
     def __init__(self):
@@ -7,12 +10,30 @@ class Option():
         self.opt = None
 
     def _initial(self):
-        self.parser.add_argument("--input_dir", type=str, default="dataset/", help="解析するデータのフォルダ")
-        self.parser.add_argument("--input_name", type=str, default="", help="解析するデータのファイル名")
-        self.parser.add_argument("--output_dir", type=str, default="result", help="解析結果の出力フォルダ")
-        self.parser.add_argument("--output_name", type=str, default="result", help="解析結果の出力ファイル名(拡張子なし)")
+        # ===============================================================
+        #                     I/O options
+        # ===============================================================
+        self.parser.add_argument("-id", "--input_dir", type=str, default="", help="解析するデータのフォルダ(dataset直下のフォルダ)")
+        self.parser.add_argument("-in", "--input_name", type=str, default="", help="解析するデータのファイル名")
+        self.parser.add_argument("-od", "--output_dir", type=str, default="", help="result直下に出力するフォルダ")
+        self.parser.add_argument("-on", "--output_name", type=str, default="result", help="解析結果の出力ファイル名(拡張子なし)")
+
+        # ===============================================================
+        #                     Analysis options
+        # ===============================================================
+        self.parser.add_argument("-p", "--pref", type=str, default="osaka", help="解析する都府の指定")
+
+    def _print(self):
+        print("\n==================Options=================")
+        pprint(vars(self.opt), indent=4)
+        print("==========================================\n")
 
     def parse(self):
         self._initial()
         self.opt = self.parser.parse_args()
+        self._print()
+        self.opt.input_dir = os.path.join("dataset", self.opt.input_dir) # dataset直下の前提
+        self.opt.output_dir = os.path.join("result", self.opt.output_dir) # result直下の前提
+        if self.opt.input_name != "": # 指定があれば*.csvの形に変更
+            self.opt.input_name = self.opt.input_name + ".csv"
         return self.opt
